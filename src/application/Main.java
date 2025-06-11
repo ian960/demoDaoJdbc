fe1242a035f9c0931814b60a7e73640b5a8120d6
ian <ianjudo552@gmail.com>
2025-06-11 18:18:25 -0300

    insert

--- src/application/Main.java
+++ src/application/Main.java
@@ -28,6 +28,10 @@ public class Main {
         for (Seller obj : list){
             System.out.println(obj);
         }
+        System.out.println("Test 4 seller insert");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New Id = " + newSeller.getId());
 
     }
 }
