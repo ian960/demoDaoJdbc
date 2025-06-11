4b62e3db8864892ce531f31d9623167234f84066
ian <ianjudo552@gmail.com>
2025-06-11 18:24:03 -0300

    update

--- src/application/Main.java
+++ src/application/Main.java
@@ -33,5 +33,11 @@ public class Main {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New Id = " + newSeller.getId());
 
+        System.out.println("Test 5 seller update");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
+
     }
 }
